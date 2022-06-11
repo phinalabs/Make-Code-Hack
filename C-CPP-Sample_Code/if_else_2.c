649047418899f6d180fbbb2542f932a66b5c5b5a
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-06-11 09:08:13 +0300

    update

--- C-CPP-Sample_Code/if_else_2.c
+++ C-CPP-Sample_Code/if_else_2.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
 int main() {
-	int num1;
-
-	//enter number 1
-	printf("Enter Stock:");
-	scanf("%d", &num1);
-
-	//out of stock
-	if (num1  == 0) printf("OUT OF STOCK");
-	//low stock
-	else if (num1  > 0 && num1 < 50)  printf("LOW STOCK");
-	//in stock
-	else if (num1  >= 50 && num1 <= 100) printf("IN STOCK");
-	//invalid
-	else printf("Invalid operation");
-
+	int stock;
+	while (1) {
+		//enter number 1
+		printf("\nEnter Stock:");
+		scanf("%d", &stock);
+		printf("Stock : %dV\n", stock);
+		stock = (int)stock;
+		//out of stock
+		if (stock  == 0) printf("OUT OF STOCK");
+		//low stock
+		else if (stock  > 0 && stock < 50)  printf("LOW STOCK");
+		//in stock
+		else if (stock  >= 50 && stock <= 100) printf("IN STOCK");
+		//invalid
+		else printf("Invalid operation");
+	}
 	return 0;
 }
