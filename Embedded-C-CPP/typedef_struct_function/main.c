f24d2753cd20ddbae52ba767082825015b6fddef
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-09-19 18:11:56 +0300

    Update main.c

--- Embedded-C-CPP/typedef_struct_function/main.c
+++ Embedded-C-CPP/typedef_struct_function/main.c
@@ -6,6 +6,7 @@
 int main(int argc, char *argv[]) {
 	int ans;
 	float div;
+	
 	/*values init*/
 	values_t values = {
 		.x = 6,
@@ -18,26 +19,33 @@ int main(int argc, char *argv[]) {
 		.multiplyFunc = &calcMultiply,
 		.divFunc = &calcDivision
 	};
+	
 	/*functions pointer*/
 	funcs_t *fp = &calcFuncs;
+	
 	/*std output*/
-	printf("Embeedded C >> typedef + Function + struct Pointer Example\n\n");
+	printf("Embedded C >> typedef + Function + struct Pointer Example\n\n");
 	printf("Values: x: %d and y : %d\n\n", values.x, values.y);
+	
 	/*addition*/
 	ans = fp->addFunc(&values);
 	printf("Addition: %d\n", ans);
+	
 	/*substraction*/
 	ans = fp->subFunc(&values);
 	printf("Substraction: %d\n", ans);
-	/*using coumpound literals*/
+	
+	/*change values using coumpound literals*/
 	values= (values_t){
 		.x = 9,
 		.y = 4
 	};
 	printf("\nValues: x: %d and y : %d\n\n", values.x, values.y);
+	
 	/*multiply*/
 	ans = fp->multiplyFunc(&values);
 	printf("Multiply: %d\n", ans);
+	
 	/*division*/
 	div = fp->divFunc(&values);
 	printf("Divide: %.2f\n", div);
