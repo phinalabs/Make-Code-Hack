d13507b1577e75f84b41c0aad193a5f8b0d408d9
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-09-22 17:28:44 +0300

    Update main.c

--- Embedded-C-CPP/malloc/main.c
+++ Embedded-C-CPP/malloc/main.c
@@ -2,13 +2,18 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+/*array size*/
+#define ARRAY_SIZE 10
+
+/*function- test malloc*/
 uint8_t func_malloc()
 {
-    int i = 0;
-
+    uint8_t i = 0;
     printf("\nAllocate mem space\n");
     /*create memory space*/
-    int *p = (int *)malloc(sizeof(int) * ARRAY_SIZE);
+    uint8_t *p = (uint8_t *)malloc(sizeof(uint8_t) * ARRAY_SIZE);
+    /*check if an error occured*/
+    if(p == NULL) return 0;
     /*set values */
     for (; i < 10; i++)
     {
@@ -21,14 +26,14 @@ uint8_t func_malloc()
     }
     /*free up created memory space*/
     free(p);
+    /*success*/
+    return 1;
 }
-/*array size*/
-#define ARRAY_SIZE 10
+
 
 /*main function*/
 int main(int argc, char *argv[])
 {
     func_malloc();
-
     return 0;
 }
