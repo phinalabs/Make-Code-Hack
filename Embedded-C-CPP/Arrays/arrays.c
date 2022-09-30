a6ad2bc83114988644cbf2642334c38f68374f27
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-09-30 08:23:07 +0300

    Update arrays.c

--- Embedded-C-CPP/Arrays/arrays.c
+++ Embedded-C-CPP/Arrays/arrays.c
@@ -10,23 +10,46 @@
 #include <time.h>
 
 #define ARRAY_LEN 4
+/**
+ * @brief Create a 1d array 1
+ * using a compile time constant
+ */
 void create_1d_array_1()
 {
-    uint8_t array[ARRAY_LEN] = {10, 20, 30, 40};
+    /*create and initialize array*/
+    int array[ARRAY_LEN] = {10, 20, 30, 40};
+    /*print array values*/
     for (int i = 0; i < ARRAY_LEN; i++) printf("%d ", array[i]);
 }
 
+/**
+ * @brief Create a 1d array 2
+ *  using a local constant
+ */
 void create_1d_array_2()
 {
+    /*array length*/
     const uint8_t array_length = 4;
-    uint8_t array[array_length];
+    /*create array*/
+    int array[array_length];
+    /*optional - init array using memset*/
     memset(array, 0,  sizeof(array_length) * array_length);
-    for (int i = 0; i < array_length; i++) printf("%d", array[i]);
+    /*seed the random generator with time*/
+    srand(time(NULL));
+    /*set array values with random numbers*/
+    for (int i = 0; i < array_length; i++) array[i] = rand();
+    /*print array values*/
+    for (int i = 0; i < array_length; i++) printf("%d ", array[i]);
 }
 
 
 int main(int argc, char *argv[])
 {
+    printf("\nArray using compile time constant\n");
     create_1d_array_1();
+    
+    printf("\nArray using local constant\n");
+    create_1d_array_2();
+    /*end*/
     return 0;
 }
