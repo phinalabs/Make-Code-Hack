874e3275b7ec6936ef8b0671bd99dfa97821b5b6
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-09-30 12:32:46 +0300

    Update array_1d.c

--- Embedded-C-CPP/Arrays/array_1d.c
+++ Embedded-C-CPP/Arrays/array_1d.c
@@ -10,7 +10,7 @@
 #include <time.h>
 
 /*array length*/
-#define  ARRAY_SIZE 4
+#define ARRAY_SIZE 4
 
 /**
  * @brief Create a 1d array 1
@@ -21,7 +21,8 @@ void create_1d_array_1()
     /*create and initialize array*/
     int array[ARRAY_SIZE] = {10, 20, 30, 40};
     /*print array values*/
-    for (int i = 0; i < ARRAY_SIZE; i++) printf("%d ", array[i]);
+    for (int i = 0; i < ARRAY_SIZE; i++)
+        printf("%d ", array[i]);
 }
 
 /**
@@ -35,22 +36,23 @@ void create_1d_array_2()
     /*create array*/
     int array[ARRAY_LENGTH];
     /*optional - init array using memset*/
-    memset(array, 0,  sizeof(*array) * ARRAY_LENGTH);
+    memset(array, 0, sizeof(*array) * ARRAY_LENGTH);
     /*seed the random generator with time*/
     srand(time(NULL));
     /*set array values with random numbers*/
-    for (int i = 0; i < ARRAY_LENGTH; i++) array[i] = rand();
+    for (int i = 0; i < ARRAY_LENGTH; i++)
+        array[i] = rand();
     /*print array values*/
-    for (int i = 0; i < ARRAY_LENGTH; i++) printf("%d ", array[i]);
+    for (int i = 0; i < ARRAY_LENGTH; i++)
+        printf("%d ", array[i]);
 }
-
 
 int main(int argc, char *argv[])
 {
     printf("Embedded C: Arrays 1d\n");
     printf("\ncreate array  using compile time constant\n");
     create_1d_array_1();
-    
+
     printf("\ncreate runtime-bound array\n");
     create_1d_array_2();
     /*end*/
